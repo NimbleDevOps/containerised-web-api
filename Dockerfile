e0e926a1e1a7ccf9284541eb064c3a11e2a52e42
mashakir1 <muhammad.shakir@nimbleapproach.com>
2022-04-13 09:33:08 +0100

    Added web api

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+WORKDIR /app
+EXPOSE 5099
+
+ENV ASPNETCORE_URLS=http://*:5099
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["containerised-web-api.csproj", "./"]
+RUN dotnet restore "containerised-web-api.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "containerised-web-api.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "containerised-web-api.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "containerised-web-api.dll"]
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Containerised Web API",
+        Description = "A Web API that display weather forecast",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
